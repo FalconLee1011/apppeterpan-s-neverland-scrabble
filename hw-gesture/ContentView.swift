395545dfcd698a99d9f8980c70b21c274412c59d
falcon 草 <falcon.lee1011@gmail.com>
2021-04-10 15:16:11 +0800

    Initial Commit

---
+++ hw-gesture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw-gesture
+//
+//  Created by falcon on 2021/4/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw-gesture/hw_gestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw_gestureApp.swift
+//  hw-gesture
+//
+//  Created by falcon on 2021/4/10.
+//
+
+import SwiftUI
+
+@main
+struct hw_gestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
